8613be99e4b4ee4911b532a0b8f75210388e249b
prajktaRaut <prajktasraut16@gmail.com>
2019-11-25 15:49:10 +0530

    All use cases of user Registration is done

---
+++ src/test/java/com/userregistration/UserRegistration.java
@@ -0,0 +1,76 @@
+package com.userregistration;
+
+import com.userregistartion.FirstNameValidator;
+import com.userregistartion.LastNameValidator;
+import com.userregistartion.MobileNumberValidator;
+import com.userregistartion.PasswordValidator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistration {
+
+    FirstNameValidator firstNameValidator=new FirstNameValidator();
+
+    LastNameValidator lastNameValidator=new LastNameValidator();
+
+    MobileNumberValidator mobileNumberValidator= new MobileNumberValidator();
+
+    PasswordValidator passwordValidator=new PasswordValidator();
+
+    @Test
+    public void CheckFirstName_IfValid_ShouldReturnTrue() {
+
+        boolean result=firstNameValidator.checkFirstName("Prajkta");
+
+        Assert.assertTrue(result);
+
+
+    }
+
+    @Test
+    public void CheckFirstName_IfNotValid_ShouldReturnFalse() {
+
+        boolean result=firstNameValidator.checkFirstName("Pr");
+
+        Assert.assertFalse(result);
+
+
+    }
+
+    @Test
+    public void CheckLastName_IfValid_ShouldReturnTrue() {
+
+        boolean result=lastNameValidator.validateName("Raut");
+
+        Assert.assertTrue(result);
+
+
+    }
+
+    @Test
+    public void CheckLastName_IfNotValid_ShouldReturnFalse() {
+
+        boolean result=lastNameValidator.validateName("Ra");
+
+        Assert.assertFalse(result);
+
+
+    }
+
+    @Test
+    public void checkMobileNumber_IfValid_ShouldReturnTrue() {
+
+        boolean result=mobileNumberValidator.validateMobileNumber("91 9850178109");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void checkMobileNumber_IfNotValid_ShouldReturnFalse() {
+
+        boolean result=mobileNumberValidator.validateMobileNumber("9850178109");
+        Assert.assertFalse(result);
+    }
+
+
+
+}
